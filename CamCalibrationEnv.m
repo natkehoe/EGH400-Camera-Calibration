d810ad0b6170f1e43c3a476a7b0c9a1dc7539f0d
natkehoe <nhk.jes@gmail.com>
2025-03-14 16:38:49 +1000

    Cam calibration calculations - Checkerboard should be 1.1x1.1 metres

---
+++ CamCalibrationEnv.m
@@ -0,0 +1,19 @@
+%% Calculating the area covered by the camera based on init 
+clc, clear all, close all
+
+load init.mat
+
+% Calculate area covered by camera
+thetas = cam.FoV/2;
+height = h1;
+xy = (height*atan(thetas)) * 2;
+
+area = xy(1)*xy(2);
+
+% Calculate how large image must be
+area20percent = area*0.2;
+minDimCheckerboard = sqrt(area20percent);
+
+disp(['Camera covers an area of ', num2str(xy(1)) ' x ' num2str(xy(2)) ' metres.'])
+disp(['Total area = ', num2str(area) ' metres. Therefore 20% of image is ' num2str(area*0.2) ' metres.'])
+disp(['Checkerboard must be at least ' num2str(minDimCheckerboard) ' metres width.'])
---
+++ CamCalibrationInit.m
@@ -0,0 +1,21 @@
+%% INIT - an outline of the camera calibration setup 
+%% determining how large the ARUCO and CHECKERBOARD markers will need to be for effective testing
+
+poseCam = [0,0,2800];
+poseOrigin = [0,0,0];
+
+
+h1 = 2.8; % ground to cam - initial guess = 2.8 [m]
+h2 = 0; % ground to 
+
+
+%% Camera specs - Intel RealSense L515
+% https://www.intelrealsense.com/lidar-camera-l515/
+cam = struct;
+cam.Pose = poseCam;
+cam.FoV = deg2rad([70, 43]); % [horizontal x vertical] [rad]
+cam.Res = [1920, 1080];
+
+
+%% Save data
+save init.mat
