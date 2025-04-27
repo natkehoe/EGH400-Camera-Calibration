8204b05f5e4bd86c6b6b9106cf6b3a9f52cce42b
natkehoe <nhk.jes@gmail.com>
2025-04-28 08:54:59 +1000

    Found covariance for filter

--- ErrorEstimateCalculations/ArucoError.m
+++ ErrorEstimateCalculations/ArucoError.m
@@ -1,6 +1,6 @@
 clc; clear all; close all
 
-testNo = 5;
+testNo = 2;
 testSelect = {"Old depth",...
             "Static - Central", ...
             "Static - Edge", ...
@@ -9,13 +9,13 @@ testSelect = {"Old depth",...
             "Rotations" ...
             };
 
-% add file directories
+% add file directories to rosbags with Aruco-Optitrack recordings
 testBagFile = {...
     "", ...
+    "C:\Users\nhkje\EGH400\Aruco ROSBAG Recordings\20250424-static\20250424-static_0.db3", ...
     "", ...
     "", ...
-    "", ...
-    "C:\Users\nhkje\EGH400\ROSBAG Recordings\20250418_arucobag-xy2\20250418_arucobag-xy2_0.mcap", ...
+    "C:\Users\nhkje\EGH400\Aruco ROSBAG Recordings\20250424-xy\20250424-xy_0.db3", ...
     "" ...
 };
 
@@ -30,58 +30,106 @@ bagReader = ros2bagreader(testBagFile{testNo});
 timeStart = bagReader.StartTime; % bag start time
 optiTopic = select(bagReader, "Topic", "/vrpn_client_node/tronpi21/pose");
 optiMsgs = readMessages(optiTopic);
-optiMsgs_time = optiTopic.MessageList.Time - timeStart;
 
 arucoTopic = select(bagReader, "Topic", "/rover_centre");
 arucoMsgs = readMessages(arucoTopic);
+
+% get messages that have the same timestamp (not doing; not possible)
+
+
+
+%% "Downsample" OptiTrack data
+optiMsgs_time = optiTopic.MessageList.Time - timeStart;
+optiTimeMasked = ones(size(optiMsgs_time));
 arucoMsgs_time = arucoTopic.MessageList.Time - timeStart;
 
-% get messages that have the same timestamp
+optiMsgs_time2 = nan(size(arucoMsgs_time)); % new time measurement to compare against arucoMsgs
+optiMsgs_select = [];
+
+% find nearest optiMsgs_time's to arucoMsgs_time (no doubling)
+for i = 1:length(arucoMsgs_time)
+    % Find OptiTrack timestamp closest to arucoMsgs
+    [~, I] = min(abs(optiMsgs_time.*optiTimeMasked - arucoMsgs_time(i)));
+    
+    % add to t
+    optiMsgs_time2(i) = optiMsgs_time(I);
+    optiMsgs_select(i) = I;
+
+    % if duplicate values, remove from search
+    % optiTimeMasked(I) = 0;
+end
 
 
-N = 10; % size of rosbag
-% get time rosbag variable
-t = [];
+% get optiTrack new data
+figure(100), clf, title("Time downsampling difference"), hold on
+plot(arucoMsgs_time, arucoMsgs_time - optiMsgs_time2)
+xlabel("Time (s)"), ylabel("Time difference (s)")
+
+%% Extract pose
+
+% Select optitrack points based on sampled time
 % get optitrack BASE LINK transform
-true_xyz = [cellfun(@(msg) msg.pose.position.x, optiMsgs), ...
-            cellfun(@(msg) msg.pose.position.y, optiMsgs), ...
-            cellfun(@(msg) msg.pose.position.z, optiMsgs)];
-% get optitrack BASE LINK rotation matrix
-true_rpy = [cellfun(@(msg) msg.pose.orientation.x, optiMsgs), ...
-            cellfun(@(msg) msg.pose.orientation.y, optiMsgs), ...
-            cellfun(@(msg) msg.pose.orientation.z, optiMsgs), ...
-            cellfun(@(msg) msg.pose.orientation.w, optiMsgs)];
-% get rover_centre output
-estimate_xyz = [cellfun(@(msg) msg.pose.position.x, arucoMsgs), ...
-                cellfun(@(msg) msg.pose.position.y, arucoMsgs), ...
-                cellfun(@(msg) msg.pose.position.z, arucoMsgs)];
-estimate_rpy = [cellfun(@(msg) msg.pose.orientation.x, arucoMsgs), ...
-                cellfun(@(msg) msg.pose.orientation.y, arucoMsgs), ...
-                cellfun(@(msg) msg.pose.orientation.z, arucoMsgs), ...
-                cellfun(@(msg) msg.pose.orientation.w, arucoMsgs)];
+% true_xyz = [cellfun(@(msg) msg.pose.position.x, optiMsgs), ...
+%             cellfun(@(msg) msg.pose.position.y, optiMsgs), ...
+%             cellfun(@(msg) msg.pose.position.z, optiMsgs)];
+% % get optitrack BASE LINK rotation matrix
+% true_quat = [cellfun(@(msg) msg.pose.orientation.x, optiMsgs), ...
+%             cellfun(@(msg) msg.pose.orientation.y, optiMsgs), ...
+%             cellfun(@(msg) msg.pose.orientation.z, optiMsgs), ...
+%             cellfun(@(msg) msg.pose.orientation.w, optiMsgs)];
+% % select only the ones we want
+% true_xyz = true_xyz(optiMsgs_select,:);
+% true_quat = true_quat(optiMsgs_select,:);
+trueData = ConvertMsgsToMat(optiMsgs(optiMsgs_select));
 
-%% If any NaN values for time, fix by setting to 0 or NAN
+% get /rover_centre output
+% estimate_xyz = [cellfun(@(msg) msg.pose.position.x, arucoMsgs), ...
+%                 cellfun(@(msg) msg.pose.position.y, arucoMsgs), ...
+%                 cellfun(@(msg) msg.pose.position.z, arucoMsgs)];
+% estimate_quat = [cellfun(@(msg) msg.pose.orientation.x, arucoMsgs), ...
+%                 cellfun(@(msg) msg.pose.orientation.y, arucoMsgs), ...
+%                 cellfun(@(msg) msg.pose.orientation.z, arucoMsgs), ...
+%                 cellfun(@(msg) msg.pose.orientation.w, arucoMsgs)];
+estData = ConvertMsgsToMat(arucoMsgs);
+
 
 %% Calculate error
 % Calculate xyz error
-err_x = abs(true_xyz(:,1) - estimate_xyz(:,1));
-err_y = abs(true_xyz(:,2) - estimate_xyz(:,2));
-err_z = abs(true_xyz(:,3) - estimate_xyz(:,3));
+err_x = estData.xyzMat(:,1) - trueData.xyzMat(:,1);
+err_y = estData.xyzMat(:,2) - trueData.xyzMat(:,2);
+err_z = estData.xyzMat(:,3) - trueData.xyzMat(:,3);
 
 EucError = sqrt(err_x.^2 + err_y.^2 + err_z.^2);
 
 
 % Calculate rpy error
-err_r = abs(true_rpy(:,1) - estimate_rpy(:,1));
-err_p = abs(true_rpy(:,2) - estimate_rpy(:,2));
-err_y = abs(true_rpy(:,3) - estimate_rpy(:,3));
+err_roll = abs(trueData.rpyMat(:,1) - estData.rpyMat(:,1));
+err_pitch = abs(trueData.rpyMat(:,2) - estData.rpyMat(:,2));
+err_yaw = abs(trueData.rpyMat(:,3) - estData.rpyMat(:,3));
 
-%% FIGURES
+%% Find mean error
+mean_xyz = mean([err_x, err_y, err_z])
+mean_rpy = mean([err_roll, err_pitch, err_yaw])
+
+% truexyzCov = ComputeCovarianceMat(trueData.xyzMat)
+% estxyzCov = ComputeCovarianceMat(estData.xyzMat)
+
+cov_xyz = ComputeCovarianceMat([err_x, err_y, err_z]) % [metres]
+cov_rpy = ComputeCovarianceMat([err_roll, err_pitch, err_yaw]) % [radians]
+
+%% Get 
+
+% subtract mean
+estData.xyzMat = estData.xyzMat - mean_xyz;
+
+test = ConvertMsgsToMat(optiMsgs(optiMsgs_select))
+
+% FIGURES
 % Euclidean Distance Error of Localisation System - [test name]
 figure(1), clf
 title(sprintf("Euclidean Distance Error of Localisation System (%s)", testSelect{testNo}))
 grid on, hold on
-plot(t, EucError)
+plot(optiMsgs_time2, EucError)
 xlabel("Time (s)"), ylabel("Euclidean Error (m)")
 
     % if roll, show roll-pitch-yaw change over time
@@ -90,34 +138,34 @@ xlabel("Time (s)"), ylabel("Euclidean Error (m)")
 figure(2), clf
 title(sprintf("Error in Individual Axes (%s)", testSelect{testNo}))
 grid on, hold on
-plot(t, err_x, 'r')
-plot(t, err_y, 'g')
-plot(t, err_z, 'b')
+plot(arucoMsgs_time, err_x, 'r')
+plot(arucoMsgs_time, err_y, 'g')
+plot(arucoMsgs_time, err_z, 'b')
 legend("X error", "Y error", "Z error")
 xlabel("Time (s)"), ylabel("Euclidean Error (m)")
 
 % True vs estimated
 figure(3), clf
-subplot(4,1), hold on
+subplot(4,1,1), hold on
     title("X Position Comparison")
-    plot(t,true_xyz(:,1))
-    plot(t, estimate_xyz(:,1))
+    plot(arucoMsgs_time,trueData.xyzMat(:,1))
+    plot(arucoMsgs_time, estData.xyzMat(:,1))
     legend("OptiTrack", "ArUco Marker")
     xlabel("Time (s)"), ylabel("X Position (m)")
-subplot(4,2), hold on
+subplot(4,1,2), hold on
     title("Y Position Comparison")
-    plot(t,true_xyz(:,2))
-    plot(t, estimate_xyz(:,2))
+    plot(arucoMsgs_time,trueData.xyzMat(:,2))
+    plot(arucoMsgs_time, estData.xyzMat(:,2))
     legend("OptiTrack", "ArUco Marker")
     xlabel("Time (s)"), ylabel("Y Position (m)")
-subplot(4,3), hold on
+subplot(4,1,3), hold on
     title("Z Position Comparison")
-    plot(t,true_xyz(:,3))
-    plot(t, estimate_xyz(:,3))
+    plot(arucoMsgs_time,trueData.xyzMat(:,3))
+    plot(arucoMsgs_time, estData.xyzMat(:,3))
     legend("OptiTrack", "ArUco Marker")
     xlabel("Time (s)"), ylabel("Z Position (m)")
-subplot(4,4), hold on
-    plot(t, EucError), legend("Euclidean Error")
+subplot(4,1,4), hold on
+    plot(arucoMsgs_time, EucError), legend("Euclidean Error")
 
 
 % Unique test figures
@@ -126,9 +174,9 @@ switch testNo
         % if depth test, also show change in euclidean error over z-distance
         figure(4), clf
         title("Error in axes due to change in depth")
-        plot(true_xyz(:,3), err_x, 'r')
-        plot(true_xyz(:,3), err_y, 'g')
-        plot(true_xyz(:,3), err_z, 'b')
+        plot(trueData.xyzMat(:,3), err_x, 'r')
+        plot(trueData.xyzMat(:,3), err_y, 'g')
+        plot(trueData.xyzMat(:,3), err_z, 'b')
         legend("X error", "Y error", "Z error")
         % Important to consider all axes errors... tells us the amount
         % of error it generally has the further away / smaller the marker is
@@ -138,18 +186,47 @@ switch testNo
         figure(5), clf
         sgtitle(sprintf("Change in XY Directions (%s)")), hold on
         subplot(2,2,1), hold on
-            plot(true_xyz(:,1), true_xyz(:,2))
-            plot(estimate_xyz(:,1), estimate_xyz(:,2))
+            plot(trueData.xyzMat(:,1), trueData.xyzMat(:,2))
+            plot(estData.xyzMat(:,1), estData.xyzMat(:,2))
             xlabel("Position (m)"), ylabel("Y Position (m)")
         subplot(2,2,2), hold on
-            plot(err_xyz(:,2), true_xyz(:,2))
+            plot(err_xyz(:,2), trueData.xyzMat(:,2))
             xlabel("Y Error")
 
         subplot(2,2,3), hold on
-            plot(err_xyz(:,1), true_xyz(:,1))
+            plot(err_xyz(:,1), trueData.xyzMat(:,1))
             xlabel("X Error")
 
 
 
 end
 
+function x = ConvertMsgsToMat(rosMsgs)
+    x.xyzMat = [cellfun(@(msg) msg.pose.position.x, rosMsgs), ...
+                cellfun(@(msg) msg.pose.position.y, rosMsgs), ...
+                cellfun(@(msg) msg.pose.position.z, rosMsgs)];
+    x.quatMat = [cellfun(@(msg) msg.pose.orientation.x, rosMsgs), ...
+                 cellfun(@(msg) msg.pose.orientation.y, rosMsgs), ...
+                 cellfun(@(msg) msg.pose.orientation.z, rosMsgs), ...
+                 cellfun(@(msg) msg.pose.orientation.w, rosMsgs)];
+    x.rpyMat = rotvec(quaternion(x.quatMat));
+
+
+end
+
+function covarianceMat = ComputeCovarianceMat(x)
+    % Params should be a matrix of format (lengthOverTime) x (sumOfVariables).
+
+    xWidth = width(x);
+    % xLength = length(x);
+    % 
+    % xMean = mean(x, 1); % find mean for each column
+
+    xVars = var(x);
+
+    covarianceMat = eye(xWidth) .* xVars;
+    
+    
+    
+    
+end
